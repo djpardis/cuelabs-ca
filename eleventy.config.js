6953392aeb4868c090d8770741e7165493561abd
djpardis <pardis.noorzad@gmail.com>
2026-09-18 16:13:21 -0700

    Rebuild as Eleventy site matching usecueport structure

---
+++ eleventy.config.js
@@ -0,0 +1,19 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/static": "/" });
+  eleventyConfig.addPassthroughCopy("src/CNAME");
+
+  eleventyConfig.addFilter("year", () => new Date().getFullYear());
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+    templateFormats: ["njk", "md", "html"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+  };
+};
